c5267ded8ca773336a340700f238a3134ccb3baf
KJstingray <kacper.j@itstartechs.com>
2024-07-20 02:48:29 +0200

    20.07 KJ

--- code/line/lineCollisions.gd
+++ code/line/lineCollisions.gd
@@ -1,14 +1,25 @@
 extends Area2D
 
-
+@onready var sigil = preload("res://scenes/projectiles/SanguineSigil.tscn")
+signal creation_finished
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (get_children().size()):
-		
-		var overlaps = get_overlapping_areas()
-		print(overlaps)
+	pass
+
+func createSigil(index):
+	var n = index
+	var vectors = []
+	while n < get_children().size() - 2:
+		vectors.append(get_children()[n].shape.a)
+		n += 1
+	var instance = sigil.instantiate()
+	instance.vectors = vectors
+	get_node("/root").add_child(instance)
+	creation_finished.emit()
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
--- code/player/player.gd
+++ code/player/player.gd
@@ -4,6 +4,9 @@ var SPEED = 100
 var TURNING_SPEED = 2
 var vectorX = 1
 var vectorY = 0
+signal sigilClosed(index)
+var wait = false
+@onready var timer= $SigilTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,24 +16,40 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_pressed("turn_right"):
-		# rotor += delta
 		vectorX += delta * TURNING_SPEED if vectorX < 1 else 0
 	if Input.is_action_pressed("turn_left"):
-		# rotor += delta
 		vectorX -= delta * TURNING_SPEED if vectorX > -1 else 0
 	if Input.is_action_pressed("turn_up"):
-		# rotor += delta
 		vectorY -= delta * TURNING_SPEED if vectorY > -1 else 0
 	if Input.is_action_pressed("turn_down"):
 		vectorY += delta * TURNING_SPEED if vectorY < 1 else 0
-		# rotor += delta
 	velocity = SPEED*Vector2(vectorX,vectorY)
-	#print(velocity)
 	velocity.limit_length(1)
-	move_and_slide()
-	#print($PathArea.get_overlapping_areas())
+	var collision = move_and_collide(velocity/50)
+	if collision:
+		print(collision.get_normal())
+		vectorX = velocity.bounce(collision.get_normal()).limit_length(1).x
+		vectorY = velocity.bounce(collision.get_normal()).limit_length(1).y
+		if collision.get_collider() is RigidBody2D:
+			collision.get_collider().apply_force(collision.get_normal() * -SPEED/5)
 	
 
 
 func _on_area_2d_body_entered(body):
 	print(body)
+
+
+func _on_path_area_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	if(!wait):
+		print('cjuu')
+		sigilClosed.emit(area_shape_index)
+		wait = true
+
+
+func _on_line_collisions_creation_finished():
+	print('chuj')
+	timer.start()
+
+
+func _on_sigil_timer_timeout():
+	wait = false
