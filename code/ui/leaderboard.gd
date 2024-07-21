fc6c3108c7fc080db2523e174ba06ca4b9368d96
somnys <108235461+somnys@users.noreply.github.com>
2024-07-21 22:59:27 +0200

    leaderboard update

--- code/ui/leaderboard.gd
+++ code/ui/leaderboard.gd
@@ -2,10 +2,9 @@ extends Control
 
 @onready var http_client = $HTTPRequest
 @onready var v_box_container = $Panel/ScrollContainer/VBoxContainer
+@onready var loading = $Panel/Loading
 
 var template_panel = preload("res://scenes/ui/leaderboard_panel.tscn")
-
-var loading = true
 
 func _ready():
 	http_client.request_completed.connect(self._on_request_completed)
@@ -16,10 +15,11 @@ func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
+	loading.visible = false
 
 	for i in range(len(response)):
 		var new_panel = template_panel.instantiate()
-		new_panel.get_node('Panel/HBoxContainer/Place').text = str(i + 1) + '#'
+		new_panel.get_node('Panel/HBoxContainer/Place').text = str(i + 1) + '.'
 		new_panel.get_node('Panel/HBoxContainer/Name').text = str(response[i]['name'])
 		new_panel.get_node('Panel/HBoxContainer/Score').text = str(response[i]['score'])
 		v_box_container.add_child(new_panel, true)
